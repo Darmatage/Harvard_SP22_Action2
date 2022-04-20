a5013afa70cbff3ac2981a5b0e8a158bb0c1a0b0
letmeeatkaik <kak2594@g.harvard.edu>
2022-04-20 12:00:59 -0400

    player

--- Harvard_Action2/Assets/Scripts/Player_Movement/Movement_arrows_in_space.cs
+++ Harvard_Action2/Assets/Scripts/Player_Movement/Movement_arrows_in_space.cs
@@ -24,28 +24,28 @@ public class Movement_arrows_in_space: MonoBehaviour {
     {
 		h = 1;
         rigidbody2d = GetComponent<Rigidbody2D>();
-		print("starting script!");
+		// print("starting script!");
     }
 	
 	 void OnCollisionEnter2D(Collision2D collision)
     {
-		print("entered a collision");//  + collision.gameObject.tag == "platform");
+		// print("entered a collision");//  + collision.gameObject.tag == "platform");
 		if (collision.gameObject.tag == "platform")
 		{
-			print("object is PLATFORM! NAME: " + collision.gameObject.name);
-			print("the tag of obj is " + collision.gameObject.tag);
+			// print("object is PLATFORM! NAME: " + collision.gameObject.name);
+			// print("the tag of obj is " + collision.gameObject.tag);
 			isGrounded = true;
 		}
 		if (collision.gameObject.tag == "grabbable")
 		{
-			print("entered GRABBABLE collision and tag is " + collision.gameObject.name);
-			print("the tag of obj is " + collision.gameObject.tag);
+			// print("entered GRABBABLE collision and tag is " + collision.gameObject.name);
+			// print("the tag of obj is " + collision.gameObject.tag);
 			Physics2D.IgnoreCollision(collision.collider, GetComponent<Collider2D>()); // player.collider);
 		}	
 		if (collision.gameObject.tag == "kill")
 		{
-			print("object is spike or fire! NAME: " + collision.gameObject.name);
-			print("the tag of obj is " + collision.gameObject.tag);
+			// print("object is spike or fire! NAME: " + collision.gameObject.name);
+			// print("the tag of obj is " + collision.gameObject.tag);
 			dragCanvasHereOxyHealth.Die();
 			
 		}
@@ -56,7 +56,7 @@ public class Movement_arrows_in_space: MonoBehaviour {
     {
 		
 		h = Input.GetAxisRaw("Horizontal");
-			print("EXITED a collision and tag is " + collision.gameObject.tag + " h on exit " + h);// + collision.gameObject.tag == "platform");
+			// print("EXITED a collision and tag is " + collision.gameObject.tag + " h on exit " + h);// + collision.gameObject.tag == "platform");
 			if( collision.gameObject.tag == "platform") 
 			{
 
@@ -76,10 +76,10 @@ public class Movement_arrows_in_space: MonoBehaviour {
 			{
 
 				h = Input.GetAxisRaw("Horizontal");
-				print("the horiz direction is " + h);
+				// print("the horiz direction is " + h);
 				// velocityNow = new Vector2 (transform.position.x + (h * speed)*Time.deltaTime, transform.position.y);
 				transform.position = new Vector2 (transform.position.x + (h * speed)*Time.deltaTime, transform.position.y);
-					print("his velocity is " + velocityNow + " transform.position.x " + transform.position.x + " h " + h);
+					// print("his velocity is " + velocityNow + " transform.position.x " + transform.position.x + " h " + h);
 
 						velocityNow = new Vector2 (1 + (h * speed)*Time.deltaTime, 0.1f);
 			
