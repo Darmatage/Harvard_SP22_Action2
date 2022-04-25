3615116599696ba4f9608fc0ba0e623931263151
MineralFoliage <micahgreenleaf@gmail.com>
2022-04-25 15:19:10 -0400

    Level Foundation Event System

---
+++ Harvard_Action2/Assets/Scripts/MicahScripts/PauseMenu.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    public GameHandler gameHandler;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+}
