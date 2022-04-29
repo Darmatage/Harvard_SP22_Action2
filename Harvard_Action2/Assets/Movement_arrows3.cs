3be4bf6a80f593e31e346c947b1044877cb96a25
letmeeatkaik <kak2594@g.harvard.edu>
2022-04-29 06:50:28 -0400

    movement

---
+++ Harvard_Action2/Assets/Movement_arrows3.cs
@@ -0,0 +1,164 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement_arrows3 : MonoBehaviour
+{
+   	public float speed = 10f;
+	public float jumpPower=0.3f;
+	// private Vector2 velocityNow;
+	public OxBarScript dragCanvasHereOxyHealth;
+	private float h=0;
+	private float v=0; // jump
+	// private float c;
+	Quaternion startRotation;
+	float time;
+	// public GameObject underFeet;
+	public PlatformChecker underMyFeet;
+	// public GameObject myFeet;
+	RaycastHit2D hit;
+	// private bool isGrabbable;
+
+// stick to floor
+// Vector3 forceDirection = new Vector3(0, 0, 1);
+
+  Rigidbody2D rigidbody2d;
+
+    [Header("Bools")]
+    [SerializeField] bool isGrounded = false;
+	
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		h = 0;
+        rigidbody2d = GetComponent<Rigidbody2D>();
+		startRotation = transform.rotation;
+    }
+	
+	 void OnCollisionEnter2D(Collision2D collision)
+    {
+		
+		if (collision.gameObject.tag == "platform")
+		{
+			// isGrounded = true;
+		}	
+		if (collision.gameObject.tag == "kill")
+		{
+			// print("object is spike or fire! NAME: " + collision.gameObject.name);
+			// print("the tag of obj is " + collision.gameObject.tag);
+			dragCanvasHereOxyHealth.Die();
+			
+		}
+		if (collision.gameObject.tag == "grabbable")
+		{
+			Physics2D.IgnoreCollision(collision.collider, GetComponent<Collider2D>()); // player.collider);
+		}	
+
+					
+    }
+	void OnCollisionExit2D(Collision2D collision)
+    {
+			if( collision.gameObject.tag == "platform") 
+			{
+
+				// isGrounded = false;
+				// rigidbody2d.velocity = h * velocityNow*speed;
+			}
+	}			
+		      
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+			isGrounded = underMyFeet.isGrounded;
+		
+			if (isGrounded)
+			{
+				transform.rotation = Quaternion.Slerp(startRotation, Quaternion.identity, time);
+				time += Time.deltaTime;
+				h = Input.GetAxisRaw("Horizontal");
+				v = Input.GetAxis("Vertical");
+
+				rigidbody2d.freezeRotation = true;
+
+			}
+			else 
+			{
+				//resume the Rigidbody  rotating
+				rigidbody2d.freezeRotation = false;
+			}
+			
+		
+    }
+
+void FixedUpdate()
+{
+
+    // isGrounded = underMyFeet.isGrounded;
+	Vector3 force3d = new Vector3 (0,-1,0);
+	Vector3 force = force3d;
+	Vector2 upDir = new Vector2(1,0);
+   // Cast a ray straight down.
+   RaycastHit2D hit = Physics2D.Raycast(transform.position, -Vector2.up);
+   
+   // check if there is platform
+   // print("the hit collider is " + hit.collider.tag);
+    // if (hit.collider.tag == "platform")
+    // {
+		// isGrounded = true;
+	// }
+	// else
+	// {
+		// isGrounded = false;
+	// }
+   //  if there's a floor beneath us
+   
+   print("is hit collider null? hit.collider " +  hit.collider + " " + h + " " + v);
+   if (hit.collider != null && isGrounded == true)
+    {
+		
+		
+		Vector3 surfaceNorm3d = new Vector3(hit.normal.x, hit.normal.y, 0);
+		
+        // Project our force direction to be parallel to the floor
+        force = Vector3.ProjectOnPlane(force3d, surfaceNorm3d);
+
+        // This means the force we're adding is now following the slopes angle
+    
+    
+	Vector2 force2D = new Vector2(force.x, force.y);
+	if(h != 0 && v == 0)
+		{
+			print("move horiz " + force2D*h*speed + " h " + h + " force2d " + force2D + " speed " + speed);
+			
+						// if we're on a horizontal surface
+			if (force2D == Vector2.zero)
+			{
+				force2D = new Vector2(1,0);
+			}
+		rigidbody2d.AddForce(force2D*h*speed, ForceMode2D.Force);
+		
+
+		}
+	if (h == 0 && v == 0)
+		{
+			rigidbody2d.velocity = Vector2.zero;
+		}
+	if (v != 0)
+	{
+		 Vector2 direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+		// maybe adding to it will be with mouse
+		// rigidbody2d.AddForce(Vector2.up, ForceMode2D.Impulse);
+		rigidbody2d.AddForce(direction*jumpPower, ForceMode2D.Impulse);
+		isGrounded = false;
+		 // StartCoroutine(jump());
+	}
+	}
+
+}
+
+
+}
