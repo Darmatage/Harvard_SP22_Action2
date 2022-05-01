575fed98114e29f771fdc491339bec3436d80a12
letmeeatkaik <kak2594@g.harvard.edu>
2022-05-01 10:12:16 -0400

    fix

--- Harvard_Action2/Assets/PlatformChecker.cs
+++ Harvard_Action2/Assets/PlatformChecker.cs
@@ -18,6 +18,41 @@ public class PlatformChecker : MonoBehaviour
     {
         
     }
+	
+	void FixedUpdate()
+	{
+		RaycastHit hit;
+
+        Vector3 p1 = transform.position;
+        float distanceToObstacle = 0;
+
+        // Cast a sphere wrapping character controller 10 meters forward
+        // to see if it is about to hit anything.
+        // if (Physics.SphereCast(p1, 2, transform.forward, out hit, 10))
+        // {
+			// print("I am in spherecast " + hit.collider.tag);
+            // distanceToObstacle = hit.distance;
+			// if(hit.collider.tag == "platform")
+			// {
+				// isGrounded = true;
+			// }
+        // }
+		 Collider2D [] colliders = Physics2D.OverlapCircleAll(transform.position, 0.5f);
+		 if(colliders.Length > 1)
+		 {
+			 
+			   // should loop and find first one with tgis tag
+				foreach (Collider2D c in colliders) 
+				{
+				   if (c.tag == "platform")
+				   {
+					   
+					   isGrounded = true;
+				   }
+				}
+		 }
+	}
+	
 	public void OnTriggerEnter2D(Collider2D other) {
               if (other.gameObject.tag == "platform"){
 							
