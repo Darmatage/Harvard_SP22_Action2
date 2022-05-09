9a080dd9e3d565d3c10bf31acc2de28bc33b2a7d
letmeeatkaik <kak2594@g.harvard.edu>
2022-05-09 08:47:11 -0400

    new

---
+++ Harvard_Action2/Assets/FlipDirection_egg.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlipDirection_egg : MonoBehaviour
+{
+	public bool isGrounded = true;
+	public EscapePodMovement3 PlatformCheckerScript;
+	Rigidbody2D rb;
+	public GameObject torso;
+	Transform torsoTrans;
+
+    void Start()
+    {
+	rb = GetComponent<Rigidbody2D>();;
+	torsoTrans = torso.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		isGrounded = PlatformCheckerScript.isGrounded;
+		if(isGrounded)
+		{
+			Flip();
+		}
+    }
+	
+	void Flip2()
+	{
+		print("flip2 " + transform.rotation + " vel " + rb.velocity);
+		// Quaternion desiredRotation = Quaternion.LookRotation(rb.velocity);
+		// transform.rotation = Quaternion.Slerp(transform.rotation, desiredRotation, Time.deltaTime);
+			
+			Vector2 moveDirection = rb.velocity;
+			if(moveDirection.y > 0)
+			{
+				print("Quaternion");
+				transform.rotation = Quaternion.AngleAxis( 180, this.transform.up ) * transform.rotation;
+ 
+			}
+			if(moveDirection.y < 0)
+			{
+				print("Quaternion2");
+				transform.rotation = new Quaternion(0, 0, 0, 1);
+			}
+		
+             // float angle = Mathf.Atan2(moveDirection.y, moveDirection.x) * Mathf.Rad2Deg;
+             // transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+			 print("flip2 AFTER " + transform.rotation + " vel " + rb.velocity);
+	}
+
+	
+	// facing direction... NOTE: this ruins the oxygen script :(
+    void Flip()
+    {
+       
+        Vector3 theScale = torsoTrans.localScale;
+		if(Input.GetAxis("Horizontal") > 0)
+		{
+			print("thescale = " + theScale);
+			if(theScale.x < 0){
+			 theScale.x *= -1;
+			}
+		}
+		if(Input.GetAxis("Horizontal") < 0) 
+		{
+			theScale.x = -1*Math.Abs(theScale.x);
+		}
+        // theScale.x *= -1;
+		
+        torsoTrans.localScale = theScale;
+	}
+}
