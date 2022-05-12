47af56cb86ff2ad57330e86ac52df80dc4087348
letmeeatkaik <kak2594@g.harvard.edu>
2022-05-11 20:59:28 -0400

    new

--- Harvard_Action2/Assets/VideoManagerEnd.cs
+++ Harvard_Action2/Assets/VideoManagerEnd.cs
@@ -6,30 +6,45 @@ using UnityEngine.Video;
 
 public class VideoManagerEnd : MonoBehaviour
 {
-   // public GameObject VideoP;
-   public VideoPlayer video;
    public MusicHandler MH;
+   public VideoPlayer videoPlayer;
+   public GameObject screen;
  
  
 	void Awake()
 	{
-		video.Play();
-        video.loopPointReached += CheckOver;
+		screen.SetActive(false);
+		// video.Play();
+        // video.loopPointReached += CheckOver;
 	}
 	
 	
     void Start()
     {
-		// MH.muteLayer2();
-		// MH.muteLayer3();
-        // video = VideoP.GetComponent<VideoPlayer>();
-        // video.Play();
-        // StartCoroutine("WaitForMovieEnd");
+		MH.muteLayer2();
+		MH.muteLayer3();
+		screen.SetActive(true);
+		videoPlayer.url = System.IO.Path.Combine(Application.streamingAssetsPath, "Ending.mp4");
+		videoPlayer.SetDirectAudioMute(0,false);
+		videoPlayer.Play();
+        videoPlayer.loopPointReached += CheckOver;
     }
  
+	
+	public void startVideo ()
+	{
+		screen.SetActive(true);
+		videoPlayer.url = System.IO.Path.Combine(Application.streamingAssetsPath, "Ending.mp4");
+		videoPlayer.SetDirectAudioMute(0,false);
+		videoPlayer.Play();
+        videoPlayer.loopPointReached += CheckOver;
+	}
+ 
+
+	
 	     void CheckOver(UnityEngine.Video.VideoPlayer vp)
     {
-       SceneManager.LoadScene("WinScene");//the scene that you want to load after the video has ended.
+         SceneManager.LoadScene("WinScene");//the scene that you want to load after the video has ended.
     }
 	
 }
