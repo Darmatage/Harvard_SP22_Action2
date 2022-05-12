3c202c8c3338e8ca19f8a3ff31ea823f7527acc2
letmeeatkaik <kak2594@g.harvard.edu>
2022-05-12 17:36:41 -0400

    new

--- Harvard_Action2/Assets/eggTransformer.cs
+++ Harvard_Action2/Assets/eggTransformer.cs
@@ -7,10 +7,16 @@ using UnityEngine;
 public class eggTransformer : MonoBehaviour
 {
 	public Camera mainCamera;
+	public AudioListener mainCamAudioListener;
+	
+	
 	public GameObject AlienShipCamera;
 	public GameObject PlayerEgg; 
 	public AudioHandlerObj AHO;
 	public bool deactivateEgg = false;
+	public GameObject w1;
+	public GameObject w2;
+	public GameObject w3;
     // Start is called before the first frame update
     void Start()
     {
@@ -32,9 +38,13 @@ public class eggTransformer : MonoBehaviour
 
 						 AlienShipCamera.SetActive(true);
 						 mainCamera.enabled = false;
+						 mainCamAudioListener.enabled = false;
 						 
 						 // TURN OFF ALL SOUNDS FROM PREV PLAYER
 						 AHO.PlaySoundLoop("ox", false); // this would be only sound still on
+						 StartCoroutine(changeColorWall(w1));
+						 StartCoroutine(changeColorWall(w2));
+						 StartCoroutine(changeColorWall(w3));
               }
 		}
        }
@@ -43,7 +53,27 @@ public class eggTransformer : MonoBehaviour
 	   public void OnTriggerExit2D(Collider2D other) {
 		       if (other.gameObject.tag == "Player"){
               deactivateEgg = true;
+			  // Collider col = gameObject.GetComponent<Collider>();
+			  // col.isTrigger = false;
 			   }
+       }
+	   
+	   
+	    IEnumerator changeColorWall(GameObject wall){
+			
+				// print("CHANGING COLOR! ");
+				
+			  Renderer checkRend = wall.GetComponentInChildren<Renderer>();
+              checkRend.material.color = new Color(2.4f, 0.9f, 0.9f, 0.5f);
+			  SpriteRenderer checkRend2 = wall.GetComponentInChildren<SpriteRenderer>();
+			  checkRend2.color = new Color(2.4f, 0.9f, 0.9f, 0.5f);
+              yield return new WaitForSeconds(0.5f);
+              checkRend.material.color = Color.green;
+			  checkRend2.color = Color.green;
+			  
+			  // remove if checkpoint img changed
+			 
+			  // checkRend.color = Color.green;
        }
 
 }
