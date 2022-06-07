0858ede19624a976f1cfbf21cc268530ef5bca0b
letmeeatkaik <kak2594@g.harvard.edu>
2022-06-07 02:09:30 -0400

    egg thruster particles

--- Harvard_Action2/Assets/VideoManager.cs
+++ Harvard_Action2/Assets/VideoManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 using UnityEngine.Video;
 
 public class VideoManager : MonoBehaviour
@@ -10,10 +11,15 @@ public class VideoManager : MonoBehaviour
    public VideoPlayer videoPlayer;
    public GameObject screen;
  
+	// image fader
+	    public Image imageToFade;
+        public float alphaLevel = 0f;
+ 
  
 	void Awake()
 	{
 		screen.SetActive(false);
+		// image.SetActive(false);
 		// video.Play();
         // video.loopPointReached += CheckOver;
 	}
@@ -21,6 +27,9 @@ public class VideoManager : MonoBehaviour
 	
     void Start()
     {
+		// imageToFade.GetComponent().color = new Color(1, 1, 1, alphaLevel);
+		
+		
 		screen.SetActive(true);
 		videoPlayer.url = System.IO.Path.Combine(Application.streamingAssetsPath, "Opening.mp4");
 		videoPlayer.SetDirectAudioMute(0,false);
@@ -53,7 +62,22 @@ public class VideoManager : MonoBehaviour
     // }
 	     void CheckOver(UnityEngine.Video.VideoPlayer vp)
     {
+		 // image.SetActive(true);
+		 StartCoroutine(FadeIn(imageToFade));
          SceneManager.LoadScene("World2");//the scene that you want to load after the video has ended.
     }
+	
+        IEnumerator FadeIn(Image fadeImage){
+			print("ALPHA IS WORKING!");
+               for (int i = 0; i < 100; i++){
+						print("ALPHA IS WORKING! " + i);
+                        alphaLevel += 0.02f;
+                        
+                        fadeImage.color = new Color(1, 1, 1, alphaLevel);
+                        Debug.Log("Alpha is: " + alphaLevel);
+                }
+				yield return null;
+				// SceneManager.LoadScene("World2");//the scene that you want to load after the video has ended.
+		}
 	
 }
